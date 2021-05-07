AOSiP-Devices/device_oneplus_sm8150-common

sm8150-common: Introduce DeviceSettings
Taken from: https://github.com/AOSiP-Devices/device_oneplus_sm8150-common/tree/99e6831dc41b8b19552ef1f6ee59db7d1e0105a4/DeviceSettings

Edits after:
 - Adapted to AndroidX
 - Includes all translations from pie cycle
 - Remove DisplayModes
 - Add DC-Dimming, FPS Info tile, Refresh Rate preferences, changed paths of sys nodes to the shorter one
 - Change layout to a cleaner look
 - Nuke KeyHandler
 - Change KeyHandler class and lib overlays to DeviceSettings
 - Update SHA of Dolby apk in proprietary-files.txt
 - Make DC-Dimming and HBM QS tile reflect active status

Co-authored-by: darkobas <darkobas@gmail.com>
Co-authored-by: Max Weninger <max.weninger@gmail.com>
Co-authored-by: Vachounet <vachounet@live.fr>
Co-authored-by: kldoc <fred.berbigier@gmail.com>
Co-authored-by: Christian Oder <myself5@carbonrom.org>
Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

DeviceSettings/src/com/aosip/device/DeviceSettings/AutoRefreshRateSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aosip.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.aosip.device.DeviceSettings.DeviceSettings;
+
+public class AutoRefreshRateSwitch implements OnPreferenceChangeListener {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_AUTO_REFRESH_RATE;
+    private Context mContext;
+
+    public AutoRefreshRateSwitch(Context context) {
+        mContext = context;
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), SETTINGS_KEY, 1) == 1;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 90f);
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 60f);
+        Settings.System.putInt(mContext.getContentResolver(), SETTINGS_KEY, enabled ? 1 : 0);
+        return true;
+    }
+}

DeviceSettings/src/com/aosip/device/DeviceSettings/ModeSwitch/WideColorModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aosip.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class WideColorModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/class/drm/card0-DSI-1/native_display_wide_color_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceSettings/src/com/aosip/device/DeviceSettings/PanelSettingsActivity.java
@@ -1,0 +1,64 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aosip.device.DeviceSettings;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class PanelSettingsActivity extends Activity {
+    private PanelSettings mPanelSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mPanelSettingsFragment = new PanelSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mPanelSettingsFragment)
+                .commit();
+        } else {
+            mPanelSettingsFragment = (PanelSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceSettings/src/com/aosip/device/DeviceSettings/RefreshRateSwitch.java
@@ -1,0 +1,59 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aosip.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.aosip.device.DeviceSettings.DeviceSettings;
+
+public class RefreshRateSwitch implements OnPreferenceChangeListener {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_REFRESH_RATE;
+    private Context mContext;
+
+    public RefreshRateSwitch(Context context) {
+        mContext = context;
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 90f) == 90f;
+    }
+
+    public static void setPeakRefresh (Context context, boolean enabled) {
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 90f : 60f);
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 90f : 60f);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 90f : 60f);
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 90f : 60f);
+        return true;
+    }
+}

DeviceSettings/src/com/aosip/device/DeviceSettings/Startup.java
@@ -1,0 +1,66 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.aosip.device.DeviceSettings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.text.TextUtils;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            Utils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, value);
+    }
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        restore(SRGBModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_HBM_SWITCH, false);
+        restore(HBMModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+        restore(DCIModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
+        restore(NightModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
+        restore(WideColorModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_FPS_INFO, false);
+        if (enabled) {
+            context.startService(new Intent(context, FPSInfoService.class));
+        }
+        VibratorStrengthPreference.restore(context);
+    }
+}

mvaisakh/android_device_oneplus_sm8150-common

* Adapted to AndroidX
* Remove DisplayModes and Dolby
* Change layout to a cleaner look
* Change KeyHandler class and lib overlays to DeviceSettings
* Add DC-Dimming and Refresh Rate preferences
* Make DC-Dimming and HBM QS tile reflect active status
Squashed:
* Change the color mode preview image - https://bit.ly/2X3zpGe
Author: Daniel Solomon <solomondaniel@google.com>
Date:   Wed Jul 31 11:46:39 2019 -0700
    sm8150-common: DeviceSettings: change the color mode preview image
    Change the color mode preview image to better illustrate the difference
    between Natural and Boosted color modes.
    Taken from: https://github.com/AOSiP/platform_packages_apps_Settings/commit/eb15083e1b079402c4b3bbcafd1f040a7b33dbf4
    Bug: 138126243
    Test: manual testing on Pixel
    Change-Id: I367b383f9f2da9b2e2d5960e51c3ec7821d4915b
    (cherry picked from commit 56574d5924727ccc95a65c088375ca4b95bd8e79)
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: Anirudh Gupta <anirudhgupta109@aosip.dev>
* Restore DC-Dimming on boot - https://bit.ly/2KW4Gsg
Author: Anirudh Gupta <anirudhgupta109@gmail.com>
Date:   Fri Dec 13 00:55:22 2019 +0000
    sm8150-common: DeviceSettings: Startup: Restore DC-Dimming on boot
* HBMModeTileService: Fix HBM write value - https://bit.ly/387jwoG
Date:   Sat Dec 21 10:51:35 2019 +0000
    sm8150-common: DeviceSettings: HBMModeTileService: Fix HBM write value
* {sepolicy,rootdir}: Address denials in DeviceSettings https://bit.ly/3hCgrQB
Date:   Thu Dec 12 20:01:42 2019 +0000
    sm8150-common: {sepolicy,rootdir}: Address denials in DeviceSettings
    Guides that helped fix these:
     - https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/4/html/SELinux_Guide/rhlcommon-section-0023.html
     - https://msfjarvis.website/posts/understanding-and-resolving-selinux-denials-on-android
     - https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/4/html/SELinux_Guide/selg-part-0057.html
* Hide st

DeviceSettings/src/com/statix/device/DeviceSettings/AutoRefreshRateSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.statix.device.DeviceSettings.DeviceSettings;
+
+public class AutoRefreshRateSwitch implements OnPreferenceChangeListener {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_AUTO_REFRESH_RATE;
+    private Context mContext;
+
+    public AutoRefreshRateSwitch(Context context) {
+        mContext = context;
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getInt(context.getContentResolver(), SETTINGS_KEY, 1) == 1;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 90f);
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, 60f);
+        Settings.System.putInt(mContext.getContentResolver(), SETTINGS_KEY, enabled ? 1 : 0);
+        return true;
+    }
+}

DeviceSettings/src/com/statix/device/DeviceSettings/ModeSwitch/WideColorModeSwitch.java
@@ -1,0 +1,52 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+public class WideColorModeSwitch implements OnPreferenceChangeListener {
+
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_wide_color_mode";
+
+    public static String getFile() {
+        if (Utils.fileWritable(FILE)) {
+            return FILE;
+        }
+        return null;
+    }
+
+    public static boolean isSupported() {
+        return Utils.fileWritable(getFile());
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Utils.getFileValueAsBoolean(getFile(), false);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Utils.writeValue(getFile(), enabled ? "1" : "0");
+        return true;
+    }
+}

DeviceSettings/src/com/statix/device/DeviceSettings/PanelSettingsActivity.java
@@ -1,0 +1,64 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class PanelSettingsActivity extends Activity {
+    private PanelSettings mPanelSettingsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mPanelSettingsFragment = new PanelSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mPanelSettingsFragment)
+                .commit();
+        } else {
+            mPanelSettingsFragment = (PanelSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

DeviceSettings/src/com/statix/device/DeviceSettings/RefreshRateSwitch.java
@@ -1,0 +1,59 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceManager;
+
+import com.statix.device.DeviceSettings.DeviceSettings;
+
+public class RefreshRateSwitch implements OnPreferenceChangeListener {
+
+    public static final String SETTINGS_KEY = DeviceSettings.KEY_SETTINGS_PREFIX + DeviceSettings.KEY_REFRESH_RATE;
+    private Context mContext;
+
+    public RefreshRateSwitch(Context context) {
+        mContext = context;
+    }
+
+    public static boolean isCurrentlyEnabled(Context context) {
+        return Settings.System.getFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, 90f) == 90f;
+    }
+
+    public static void setPeakRefresh (Context context, boolean enabled) {
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 90f : 60f);
+        Settings.System.putFloat(context.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 90f : 60f);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        Boolean enabled = (Boolean) newValue;
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.PEAK_REFRESH_RATE, enabled ? 90f : 60f);
+        Settings.System.putFloat(mContext.getContentResolver(),
+                Settings.System.MIN_REFRESH_RATE, enabled ? 90f : 60f);
+        return true;
+    }
+}

DeviceSettings/src/com/statix/device/DeviceSettings/Startup.java
@@ -1,0 +1,62 @@
+/*
+* Copyright (C) 2013 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.statix.device.DeviceSettings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
+import android.text.TextUtils;
+import androidx.preference.PreferenceManager;
+
+public class Startup extends BroadcastReceiver {
+
+    private void restore(String file, boolean enabled) {
+        if (file == null) {
+            return;
+        }
+        if (enabled) {
+            Utils.writeValue(file, "1");
+        }
+    }
+
+    private void restore(String file, String value) {
+        if (file == null) {
+            return;
+        }
+        Utils.writeValue(file, value);
+    }
+
+    @Override
+    public void onReceive(final Context context, final Intent bootintent) {
+        boolean enabled = false;
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
+        restore(SRGBModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_HBM_SWITCH, false);
+        restore(HBMModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_DC_SWITCH, false);
+        restore(DCModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_DCI_SWITCH, false);
+        restore(DCIModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_WIDECOLOR_SWITCH, false);
+        restore(WideColorModeSwitch.getFile(), enabled);
+        VibratorStrengthPreference.restore(context);
+    }
+}