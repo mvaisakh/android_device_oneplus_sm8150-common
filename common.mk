2c3d9748e43b54fce7fa1e387d941417e9def0bf
Vaisakh Murali <mvaisakh@statixos.com>
2021-05-22 20:11:39 +0200

    sm8150-common: Drop local telephony-ext boot jar specification
    * Already defined in platform
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- common.mk
+++ common.mk
@@ -432,9 +432,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
